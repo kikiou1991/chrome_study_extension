9080365855ba22a7632e7d366c07832034876557
Gabor Adorjani <gadorjani@windowslive.com>
2023-09-06 21:20:24 +0100

    work-timer-imple
    created the work timer logic to count backwords from the given value...this needs to be changed later...once we can get the user input to set the timer...unless we want to create default  values

--- popups/popup.js
+++ popups/popup.js
@@ -24,10 +24,45 @@ stopBtn.addEventListener("click", () => {
     })
 })
 
-//Event lustener to reset the work timer
-stopBtn.addEventListener("click", () => {
+//Event listener to reset the work timer
+resetBtn.addEventListener("click", () => {
     chrome.storage.local.set({
         workTimer: 0,
         isWorkRunning: false
     })
 });
+
+//Function to update the work timer display
+
+function updateWorkTimerDisplay() {
+    chrome.storage.local.get(["workTimer"], (res) => {
+        const workTime = document.getElementById("work-time"); // Grab the element we need
+        let totalSeconds = 60 - res.workTimer; // Calculate the total seconds remaining
+
+        if (totalSeconds < 0) {
+            totalSeconds = 0; // Ensure the timer doesn't go below 00:00
+        }
+
+        const minutes = Math.floor(totalSeconds / 60); // Calculate the minutes
+        const seconds = totalSeconds % 60; // Calculate the seconds
+
+        const formattedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`; // Format minutes
+        const formattedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`; // Format seconds
+
+        workTime.textContent = `${formattedMinutes}:${formattedSeconds}`; // Display time
+        if (minutes === 0 && seconds === 0) {
+            stopWorkTimer();
+        }
+    })
+}
+
+updateWorkTimerDisplay();
+setInterval(updateWorkTimerDisplay, 1000)// call the function we created and increment by one second
+
+//function to stop the timer when it reaches 00:00
+function stopWorkTimer() {
+    chrome.storage.local.set({
+        workTimer: 0,
+        isWorkRunning: false,
+    })
+}
